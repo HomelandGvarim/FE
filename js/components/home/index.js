c439d58bf3c6169875fb73246b27f491ef5ce3f2
DavidShusterman <davidshusti@gmail.com>
2017-08-25 00:16:52 +0300

    update

--- js/components/home/index.js
+++ js/components/home/index.js
@@ -4,13 +4,45 @@ import { Image, View, StatusBar,TouchableHighlight,TouchableOpacity  } from "rea
 import { Container, Button, H3, Text, Header, Title, Body, Left, Right ,Toast } from "native-base";
 
 import styles from "./styles";
-
+import PushNotification from 'react-native-push-notification';
 
 const launchscreenBg = require("../../../img/gray-background.png");
 const launchscreenLogo = require("../../../img/ShieldLogo.png");
 const thumbsDownButton = require("../../../img/thumbs-down-icon.png");
 const thumbsUpButton = require("../../../img/thumbs-up-button.png");
 
+
+
+ 
+PushNotification.configure({
+ 
+    // (optional) Called when Token is generated (iOS and Android) 
+    onRegister: function(token) {
+        alert( 'TOKEN:', token );
+    },
+ 
+    // (required) Called when a remote or local notification is opened or received 
+    onNotification: function(notification) {
+        alert( 'NOTIFICATION:', notification );
+    },
+ 
+    // ANDROID ONLY: (optional) GCM Sender ID. 
+    senderID: "5555555555555",
+ 
+    // IOS ONLY (optional): default: all - Permissions to register. 
+    permissions: {
+        alert: true,
+        badge: true,
+        sound: true
+    },
+    
+    /**
+      * IOS ONLY: (optional) default: true
+      * - Specified if permissions will requested or not,
+      * - if not, you must call PushNotificationsHandler.requestPermissions() later
+      */
+    requestPermissions: true,
+});
 let notOklogo = {
     left:30,
     top:0,
@@ -61,7 +93,15 @@ class Home extends Component {
 
  }
  test(){
-	
+	return fetch("http://192.168.0.128:7916",{
+		method: 'GET',
+	}).then((response)=>{
+		return response.json()
+	}).then((res)=>{
+		alert(JSON.stringify(res));
+	}).catch((error)=>{
+		alert(error);
+	})
  }
  
 	render() {
