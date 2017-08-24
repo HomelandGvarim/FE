9332b94978be31355090f881edd8afc1c6ff33de
yuvaliz <zaif.yuval@gmail.com>
2017-08-24 23:21:55 +0300

    added gcm java shit

--- android/app/src/main/java/com/nativebasekitchensink/MainApplication.java
+++ android/app/src/main/java/com/nativebasekitchensink/MainApplication.java
@@ -3,6 +3,7 @@ package com.nativebasekitchensink;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -24,7 +25,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativePushNotificationPackage(),
             new MapsPackage(),
+
             new VectorIconsPackage()
       );
     }
