0636c47f63a18b7bf19d29d876479b481a0d26c7
yuvaliz <zaif.yuval@gmail.com>
2017-08-24 21:18:20 +0300

    fix the biggest bug ever

--- js/components/sidebar/index.js
+++ js/components/sidebar/index.js
@@ -2,22 +2,6 @@ import React, { Component } from "react";
 import { Image } from "react-native";
 
 import {
-<<<<<<< HEAD
-  Content,
-  Text,
-  List,
-  ListItem,
-  Icon,
-  Container,
-  Left,
-  Right,
-  Badge,
-  Button,
-  View,
-  StyleProvider,
-  getTheme,
-  variables
-=======
 	Content,
 	Text,
 	List,
@@ -32,7 +16,6 @@ import {
 	StyleProvider,
 	getTheme,
 	variables
->>>>>>> 6027e4131f26899250c706f57db3329a6703d46d
 } from "native-base";
 
 import styles from "./style";
@@ -216,57 +199,6 @@ class SideBar extends Component {
     };
   }
 
-<<<<<<< HEAD
-  render() {
-    return (
-      <Container>
-        <Content
-          bounces={false}
-          style={{ flex: 1, backgroundColor: "#fff", top: -1 }}
-        >
-          <Image source={drawerCover} style={styles.drawerCover}>
-            <Image square style={styles.drawerImage} source={drawerImage} />
-          </Image>
-          <List
-            dataArray={datas}
-            renderRow={data =>
-              <ListItem
-                button
-                noBorder
-                onPress={() => this.props.navigation.navigate(data.route)}
-              >
-                <Left>
-                  <Icon
-                    active
-                    name={data.icon}
-                    style={{ color: "#777", fontSize: 26, width: 30 }}
-                  />
-                  <Text style={styles.text}>
-                    {data.name}
-                  </Text>
-                </Left>
-                {data.types &&
-                  <Right style={{ flex: 1 }}>
-                    <Badge
-                      style={{
-                        borderRadius: 3,
-                        height: 25,
-                        width: 72,
-                        backgroundColor: data.bg
-                      }}
-                    >
-                      <Text
-                        style={styles.badgeText}
-                      >{`${data.types} Types`}</Text>
-                    </Badge>
-                  </Right>}
-              </ListItem>}
-          />
-        </Content>
-      </Container>
-    );
-  }
-=======
 	render() {
 		return (
 			<Container>
@@ -304,7 +236,6 @@ class SideBar extends Component {
 			</Container>
 		);
 	}
->>>>>>> 6027e4131f26899250c706f57db3329a6703d46d
 }
 
 export default SideBar;
